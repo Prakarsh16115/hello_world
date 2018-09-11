22dcb2b38e920a1c317f3cc7c9e06783bc966a67
ankita2703 <43179815+ankita2703@users.noreply.github.com>
2018-09-11 22:05:44 +0530

    added 1st file

---
+++ function4.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int add (int,int);
+void main()
+{
+    int s,x,y;
+    printf("enter two nos.");
+    scanf("%d%d",&x,&y);
+    s=add(x,y);
+    printf("sum is %d",s);
+
+}
+int add(int a, int b)
+{
+    int c;
+    c=a+b;
+    return(c);}
+
